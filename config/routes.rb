c7111b82ebdfc5b98fcd4518b3fc2d4ee50e8d46
Candice Davidson <cndragn@gmail.com>
2018-03-02 23:17:09 -0500

    create new category deck link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :decks
-  resources :categories
+  # resources :categories
   resources :scores
   resources :cards
   root 'page#index'
@@ -14,5 +14,9 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+
+  resources :categories do
+  resources :decks
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
