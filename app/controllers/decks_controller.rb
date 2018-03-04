7ba65113201044cee9bc76905cf329db82385846
Candice Davidson <cndragn@gmail.com>
2018-03-04 05:10:24 -0500

    save new deck with lanugage id

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -72,6 +72,6 @@ class DecksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def deck_params
-      params.require(:deck).permit(:language, :title, :description, :category_id, :user_id)
+      params.require(:deck).permit(:language, :title, :description, :category_id, :user_id, :language_id)
     end
 end
