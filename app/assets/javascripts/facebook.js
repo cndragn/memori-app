1a2e2489ed2f561e6cdbf51528ba258b7c28ebb9
Johanna <Johannakarlsson1@hotmail.com>
2018-03-05 22:08:41 -0500

    moved fb-login to login-page

--- app/assets/javascripts/facebook.js
+++ app/assets/javascripts/facebook.js
@@ -1,42 +1,42 @@
-window.fbAsyncInit = function() {
-    FB.init({
-      appId            : '392870361184712',
-      autoLogAppEvents : true,
-      xfbml            : true,
-      version          : 'v2.12'
-    });
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
-function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    // statusChangeCallback(response);
-    // console.log(response);
-    if (response)
-    {
-      FB.api(
-        '/me?fields=email,name',
-        function(response) {
-          console.log(response);
-          $.post({
-            url: '/users',
-            data: {
-              user: {
-                name: response.name,
-                email: response.email,
-                password: "password"
-              }
-            }
-          });
-      });
-    }
-
-  });
-}
+// window.fbAsyncInit = function() {
+//     FB.init({
+//       appId            : '392870361184712',
+//       autoLogAppEvents : true,
+//       xfbml            : true,
+//       version          : 'v2.12'
+//     });
+//   };
+//
+//   (function(d, s, id){
+//      var js, fjs = d.getElementsByTagName(s)[0];
+//      if (d.getElementById(id)) {return;}
+//      js = d.createElement(s); js.id = id;
+//      js.src = "https://connect.facebook.net/en_US/sdk.js";
+//      fjs.parentNode.insertBefore(js, fjs);
+//    }(document, 'script', 'facebook-jssdk'));
+//
+// function checkLoginState() {
+//   FB.getLoginStatus(function(response) {
+//     // statusChangeCallback(response);
+//     // console.log(response);
+//     if (response)
+//     {
+//       FB.api(
+//         '/me?fields=email,name',
+//         function(response) {
+//           console.log(response);
+//           $.post({
+//             url: '/users',
+//             data: {
+//               user: {
+//                 name: response.name,
+//                 email: response.email,
+//                 password: "password"
+//               }
+//             }
+//           });
+//       });
+//     }
+//
+//   });
+// }
