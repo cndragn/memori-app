c4415a892928c5c46811480231640a782c0da84e
Johanna <Johannakarlsson1@hotmail.com>
2018-03-10 13:53:47 -0500

    moved google-login to login-page

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20180306023055) do
+ActiveRecord::Schema.define(version: 20180306213009) do
 
   create_table "cards", force: :cascade do |t|
     t.string "original"
