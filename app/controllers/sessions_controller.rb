6e634bc1d2d0125630beb64f28aa52785c049874
Candice Davidson <cndragn@gmail.com>
2018-05-21 16:00:03 -0400

    remove social login and update api key

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,9 +10,9 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to users_path
       #google authenticate code
-    elsif user = User.from_omniauth(request.env["omniauth.auth"])
-    session[:user_id] = user.id
-    redirect_to users_path
+    # elsif user = User.from_omniauth(request.env["omniauth.auth"])
+    # session[:user_id] = user.id
+    # redirect_to users_path
     else
       # If user's login doesn't work, send them back to the login form.
       redirect_to login_path
